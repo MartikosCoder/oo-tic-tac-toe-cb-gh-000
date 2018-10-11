229294821f6b4db74d5e69fdd816a75f3e67c8a5
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2018-10-11 11:21:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,14 +85,6 @@ class TicTacToe
   end
 
   def play
-    until over?
-      turn
-    end
-
-    if won?
-      puts "Congratulations #{winner}"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+    
   end
 end
